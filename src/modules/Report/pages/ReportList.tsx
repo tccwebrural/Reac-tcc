2800d943ac5b69a6df1c21071261862ee0141d42
Letícia Santana <leticiasantanabrites@gmail.com>
2022-04-02 13:44:44 -0300

    let

--- src/modules/Report/pages/ReportList.tsx
+++ src/modules/Report/pages/ReportList.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {Button} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -14,11 +14,27 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import './ReportList.css';
+import { ReportForm } from "../models/Report.model";
+import { ReportService } from "../services/Reports.services";
 
 const ListReportPage = (): ReactElement=> {    
   
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = useState<ReportForm[]>([]);
+
+  
+  const [rowsPerPage, setRowsPerPage] =useState(10);
+  const reportService = new ReportService();
+
+  useEffect(() => {
+
+          //TODO: Implementar chamada para o backend
+         reportService.getReportById.then(
+              reports => setPage(reports)
+              
+          );
+      }, []);
+
+
 
   const handleChangePage = (event: unknown, newPage: number) => {
       setPage(newPage);
