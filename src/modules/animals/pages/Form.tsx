d403e269c1ac7fcfdf24929e795ee9ea5213ec0b
george <jogos_townsend@hotmail.com>
2022-04-01 23:35:24 -0300

    form snackbar

--- src/modules/animals/pages/Form.tsx
+++ src/modules/animals/pages/Form.tsx
@@ -1,213 +1,261 @@
 import React, { ReactElement } from "react";
-import { Box, Typography } from "@mui/material";
-import TextField from '@mui/material/TextField';
-import {Link} from 'react-router-dom'
-import './Form.css';
+import { Alert, Box, Snackbar, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+
+import "./Form.css";
+import { Link } from "react-router-dom";
 
 import { AnimalService } from "../services/Animals.service";
 
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import { useState } from "react";
-import {  Animal  } from '../models/Animal.model';
-import { AnimalService } from "../services/Animals.service";
+import { Animal } from "../models/Animal.model";
+import { checkServerIdentity } from "tls";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  margin:'auto',
-  marginTop:120,
-  maxWidth:800,
-  minheight:420, 
-  borderTop:2,
+  margin: "auto",
+  marginTop: 120,
+  maxWidth: 800,
+  minheight: 420,
+  borderTop: 2,
   color: theme.palette.text.secondary,
 }));
 
-const FormAnimalPage = (): ReactElement => {   
+interface SnackBarProps {
+  message?: string;
+  show: boolean;
+  severity?: "success" | "error";
+}
+const FormAnimalPage = (): ReactElement => {
+  const [dataForm, setDataForm] = useState<Animal>({});
 
-  const [dataForm,setDataForm] = useState<Animal>({}); 
-  const [animalForm,setAnimalForm] = useState<Animal>({}); 
+  const [snackbar, setSnackbar] = useState<SnackBarProps>({ show: false });
 
   const animalService = new AnimalService();
-  
-  const onChangeDataForm = (event:any)=>{
-    const {name, value} = event.target
-    setDataForm({ ...dataForm,[name]: value});
+
+  const onChangeDataForm = (event: any) => {
+    const { name, value } = event.target;
+    setDataForm({ ...dataForm, [name]: value });
 
     //  animalService.createAnimal(dataForm.id)
     // animalService.createAnimal(dataForm, '5ab6e784-ef43-4b67-b2b2-09745272319a')
-
-
-  }
+  };
   const salvarDadosAnimal = () => {
+    animalService
+      .createAnimal(dataForm, "d11b6b19-085d-4a3c-a9fe-3fece68f20ef")
+      .then(() => {
+        setSnackbar({
+          show: true,
+          severity: "success",
+          message: "Animal cadastro com sucesso!",
+        });
+      })
+      .catch(() => {
+        setSnackbar({
+          show: true,
+          severity: "error",
+          message: "Falha ao cadastrar o animal, tente novamente mais tarde!",
+        });
+      });
 
-//  animalService.createAnimal(dataForm,'5ab6e784-ef43-4b67-b2b2-09745272319a').then(()=>{
-//    console.log('ok')
+    console.log(dataForm);
+  };
 
-//  })
- animalService.createAnimal(dataForm,'5ab6e784-ef43-4b67-b2b2-09745272319a').then()
-
- console.log(dataForm);
-  } 
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason !== "clickaway") {
+      setSnackbar({ show: false });
+    }
+  };
 
   const showQtyChildreen = () => {
-    if (dataForm.category === 1){
-      return<>
-      <TextField
-          label="Quantidade de Cria"
-          type="number"
-          name= 'qtyChildreen'
-
-          InputLabelProps={{
-            shrink: true,
-          }}
-          value={dataForm.qtyChildreen}
-
-          onChange={onChangeDataForm}
-          // name= 'showQtyChildreen'
-         
-
-        />
-      </>
+    if (dataForm.category === 1) {
+      return (
+        <>
+          <TextField
+            label="Quantidade de Cria"
+            type="number"
+            name="qtyChildreen"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            value={dataForm.qtyChildreen}
+            onChange={onChangeDataForm}
+            // name= 'showQtyChildreen'
+          />
+        </>
+      );
     }
-  }
+  };
   const showQtyMilk = () => {
-    if(dataForm.category === 1 && dataForm.type === 2){
-   return <>
-      <FormControl sx={{ m: 1, minWidth: 220 }}>
-        <InputLabel htmlFor="qtyMilk" >Qtd de Leite por dia</InputLabel>
-          <Select  name='qtyMilk'  label="Grouping" onChange={onChangeDataForm} value={dataForm.qtyMilk}>
-            <MenuItem value={1}>1 Litro</MenuItem>
-            <MenuItem value={2}>2 Litros</MenuItem>
-            <MenuItem value={3}>3 Litros</MenuItem>
-            <MenuItem value={4}>4 Litros</MenuItem>
-            <MenuItem value={5}>5 Litros</MenuItem>
-            <MenuItem value={6}>6 Litros</MenuItem>
-            <MenuItem value={7}>7 Litros</MenuItem>
-            <MenuItem value={8}>8 Litros</MenuItem>
-            <MenuItem value={9}>9 Litros</MenuItem>
-            <MenuItem value={10}>10 Litros</MenuItem>
-            <MenuItem value={11}>À cima de 10 Litros</MenuItem>
-          </Select>
-        </FormControl>
-      </>    
+    if (dataForm.category === 1 && dataForm.type === 2) {
+      return (
+        <>
+          <FormControl sx={{ m: 1, minWidth: 220 }}>
+            <InputLabel htmlFor="qtyMilk">Qtd de Leite por dia</InputLabel>
+            <Select
+              name="qtyMilk"
+              label="Grouping"
+              onChange={onChangeDataForm}
+              value={dataForm.qtyMilk}
+            >
+              <MenuItem value={1}>1 Litro</MenuItem>
+              <MenuItem value={2}>2 Litros</MenuItem>
+              <MenuItem value={3}>3 Litros</MenuItem>
+              <MenuItem value={4}>4 Litros</MenuItem>
+              <MenuItem value={5}>5 Litros</MenuItem>
+              <MenuItem value={6}>6 Litros</MenuItem>
+              <MenuItem value={7}>7 Litros</MenuItem>
+              <MenuItem value={8}>8 Litros</MenuItem>
+              <MenuItem value={9}>9 Litros</MenuItem>
+              <MenuItem value={10}>10 Litros</MenuItem>
+              <MenuItem value={11}>À cima de 10 Litros</MenuItem>
+            </Select>
+          </FormControl>
+        </>
+      );
     }
-  }
-   return (<>
-     
-    <Box sx={{'& .MuiTextField-root': { m: 1, width: '33ch', },}}>
-      <Grid >
-        <Item id="formCadAnimal">
+  };
+  return (
+    <>
+      <Box sx={{ "& .MuiTextField-root": { m: 1, width: "33ch" } }}>
+        <Grid>
+          <Item id="formCadAnimal">
+            <h1>Cadastrar Animal</h1>
 
-        <h1>Cadastrar Animal</h1>
-        
-        <FormControl sx={{ m: 1, minWidth: 221 }}>
-          <InputLabel htmlFor="grouped-select">Categoria</InputLabel>
-          <Select label="Grouping" name="category" value={dataForm.category} onChange={onChangeDataForm} >
-            <MenuItem  value={1} >Vaca</MenuItem>
-            <MenuItem  value={2} >Boi</MenuItem>
-            <MenuItem  value={3} >Bezerra</MenuItem>
-            <MenuItem  value={4} >Bezerro</MenuItem>
-            <MenuItem  value={5} >Novilha</MenuItem>
-            <MenuItem  value={6} >Novilho</MenuItem>
-            
-          </Select>
-          
-        </FormControl>
-        
-        <TextField
-          label="Identificador"
-          type="number"
-          name = 'identifier'
+            <FormControl sx={{ m: 1, minWidth: 221 }}>
+              <InputLabel htmlFor="grouped-select">Categoria</InputLabel>
+              <Select
+                label="Grouping"
+                name="category"
+                value={dataForm.category}
+                onChange={onChangeDataForm}
+              >
+                <MenuItem value={1}>Vaca</MenuItem>
+                <MenuItem value={2}>Boi</MenuItem>
+                <MenuItem value={3}>Bezerra</MenuItem>
+                <MenuItem value={4}>Bezerro</MenuItem>
+                <MenuItem value={5}>Novilha</MenuItem>
+                <MenuItem value={6}>Novilho</MenuItem>
+              </Select>
+            </FormControl>
 
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={onChangeDataForm}
-          // name= 'identifier'
+            <TextField
+              label="Identificador"
+              type="number"
+              name="identifier"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={onChangeDataForm}
+              // name= 'identifier'
 
               // value={dataForm.identifier}
-           />
-          
-        <TextField 
-          label="Nome"
-          type="text"
-          name='name'
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={onChangeDataForm}
-          value={dataForm.name}
-        />
+            />
 
-        <TextField
-          label="Data de Nascimento"
-          type="date"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          name='birthday'
-          onChange={onChangeDataForm}
-          value={dataForm.birthday}
-        />
-       <FormControl sx={{ m: 1, minWidth: 221 }}>
-          <InputLabel htmlFor="type">Tipo</InputLabel>
-          <Select label="Grouping" name="type" value={dataForm.type} onChange={onChangeDataForm} >
-            <MenuItem  value={1} >Gado de Corte</MenuItem>
-            <MenuItem  value={2} >Gado Leitero</MenuItem>
-            
-          </Select>
-          
-        </FormControl>
-      
-      <TextField
-          label="Peso Aproximadamente"
-          name= 'weight'
-          type="number"
+            <TextField
+              label="Nome"
+              type="text"
+              name="name"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={onChangeDataForm}
+              value={dataForm.name}
+            />
 
-          InputLabelProps={{
-            shrink: true,
-          }}
-         
-          onChange={onChangeDataForm}
-          // value={dataForm.weight}
-          value = {dataForm.weight}
-          
+            <TextField
+              label="Data de Nascimento"
+              type="date"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              name="birthday"
+              onChange={onChangeDataForm}
+              value={dataForm.birthday}
+            />
+            <FormControl sx={{ m: 1, minWidth: 221 }}>
+              <InputLabel htmlFor="type">Tipo</InputLabel>
+              <Select
+                label="Grouping"
+                name="type"
+                value={dataForm.type}
+                onChange={onChangeDataForm}
+              >
+                <MenuItem value={1}>Gado de Corte</MenuItem>
+                <MenuItem value={2}>Gado Leitero</MenuItem>
+              </Select>
+            </FormControl>
 
-        />
-       {showQtyChildreen()}
-       {showQtyMilk()}
+            <TextField
+              label="Peso Aproximadamente"
+              name="weight"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={onChangeDataForm}
+              // value={dataForm.weight}
+              value={dataForm.weight}
+            />
+            {showQtyChildreen()}
+            {showQtyMilk()}
 
-        <Stack direction="row" spacing={2}>
-        
-          <Grid container justifyContent="flex-end" >
-            <Stack spacing={2} direction="row" sx={{margin:3, marginTop:8}}>
-              
-                <Button variant="contained" color="error"component={Link} to='animals/list'>
-                  Cancelar
-                </Button> 
-            
-              <Button variant="contained" color="success" onClick={salvarDadosAnimal}>
-                Salvar
-              </Button>
-              </Stack>
-            </Grid>
-        </Stack>
-        </Item>
-      </Grid>
-    </Box>
-      
+            <Stack direction="row" spacing={2}>
+              <Grid container justifyContent="flex-end">
+                <Stack
+                  spacing={2}
+                  direction="row"
+                  sx={{ margin: 3, marginTop: 8 }}
+                >
+                  <Button
+                    variant="contained"
+                    color="error"
+                    component={Link}
+                    to="animals/list"
+                  >
+                    Cancelar
+                  </Button>
+
+                  <Button
+                    variant="contained"
+                    color="success"
+                    onClick={salvarDadosAnimal}
+                  >
+                    Salvar
+                  </Button>
+
+                  <Snackbar
+                    open={snackbar.show}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                  >
+                    <Alert onClose={handleClose} severity={snackbar.severity}>
+                      {snackbar.message}
+                    </Alert>
+                  </Snackbar>
+                </Stack>
+              </Grid>
+            </Stack>
+          </Item>
+        </Grid>
+      </Box>
     </>
   );
-}
+};
 
-export default FormAnimalPage
+export default FormAnimalPage;
