f2655602dcdd0bbd87ca096cfee6b75bdc0b82e1
george <jogos_townsend@hotmail.com>
2022-04-04 18:21:52 -0300

    report service commit

--- src/modules/Report/services/Reports.services.ts
+++ src/modules/Report/services/Reports.services.ts
@@ -1,8 +1,10 @@
+import { Animal } from './../../animals/models/Animal.model';
 import { API_ANIMALS_ROUTE } from './../../../constants';
 import { ReportForm } from './../models/Report.model';
 
 import { AXIOS_CONNECTOR_API } from '../../../services/axios.service';
 import {API_REPORT_ROUTE } from '../../../constants';
+import { Params } from 'react-router-dom';
 
 /**
  * Serviço responsável por se comunicar com api de animais
@@ -11,28 +13,29 @@ export class ReportService {
 
     
 
-    getReportById(animalId: string) {
+    get_Animal_Id_Reports(animalId: string) {
         //TODO: Implementar chamada para a api para realizar a operação;
         return AXIOS_CONNECTOR_API.get(`/${API_ANIMALS_ROUTE}/${animalId}/${API_REPORT_ROUTE}`)
             .then(response => response.data)
             .catch(err => console.error(err));
     }
 
-    createReport(reportForm: ReportForm, animalId: String ) {
+    create_Report(reportForm: ReportForm, animalId: String ) {
 
         return AXIOS_CONNECTOR_API.post(`/${API_ANIMALS_ROUTE}/${animalId}/${API_REPORT_ROUTE}`, reportForm)
         .then(response => response.data)
         .catch(err=>  console.error(err))
     }
 
-    deleteReport(animalId: String) {
+    delete_Report(animalId: String) {
         
         return AXIOS_CONNECTOR_API.delete(`/${API_ANIMALS_ROUTE}/${animalId}/${API_REPORT_ROUTE}`)
         .then(response => response.data)
         .catch(err=>  console.error(err))
 
+        
     }
-    updateReport(animalId: String) {
+    update_Report(animalId: String) {
 
         return AXIOS_CONNECTOR_API.delete(`/${API_ANIMALS_ROUTE}/${animalId}/${API_REPORT_ROUTE}`)
         .then(response => response.data)
